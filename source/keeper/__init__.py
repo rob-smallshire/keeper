246d77342a2fe1addee36cc529aada9adb165ab7
Robert Smallshire <rob@sixty-north.com>
2020-11-20 14:08:50 +0100

    Arrange for top-level imports from the package

--- source/keeper/__init__.py
+++ source/keeper/__init__.py
@@ -1,6 +1,11 @@
 from .keeper import Keeper
+from .storage.writecachestorage import WriteCacheStorage
+from .storage.filestorage import FileStorage
+
 from .version import __version__, __version_info__
 
 __all__ = [
-    "Keeper"
+    "Keeper",
+    "FileStorage",
+    "WriteCacheStorage",
 ]
